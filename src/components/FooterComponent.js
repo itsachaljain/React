b4886e885dacbec9e8732187c710c5f04b893b0e
itsachaljain <achaljain785@gmail.com>
2020-08-17 16:02:51 +0530

    Assigment 2

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -13,7 +13,7 @@ function Footer(props) {
 								<Link to="/home">Home</Link>
 							</li>
 							<li>
-								<Link to="/about">About Us</Link>
+								<Link to="/aboutus">About Us</Link>
 							</li>
 							<li>
 								<Link to="/menu">Menu</Link>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import DishDetails from "./DishdetailComponent";
 import Header from "./HeaderComponent";
@@ -61,6 +62,11 @@ class Main extends Component {
 					/>
 					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route
+						exact
+						path="/aboutus"
+						component={() => <About leaders={this.state.leaders} />}
+					/>
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
