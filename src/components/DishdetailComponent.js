e9d000d6e884097e999d70787350612e00e134d3
itsachaljain <achaljain785@gmail.com>
2020-08-25 23:50:29 +0530

    Assigment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,6 +9,7 @@ import {
 	BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentFormComponent";
 
 function RenderDish({ dish }) {
 	return (
@@ -73,6 +74,9 @@ const DishDetails = (props) => {
 					</div>
 					<div className="col-12 col-md-5 m-1">
 						<RenderComments comments={props.comments} />
+						<div>
+							<CommentForm />
+						</div>
 					</div>
 				</div>
 			</div>
